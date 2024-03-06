a749be472a4afc87db9bde459b29591ee84427d8
Tsolo Khambule <tskhamb022@student.wethinkcode.co.za>
2024-03-06 12:43:16 +0200

    added functionality for guess limit

--- number_game/src/main/java/task1/example/Main.java
+++ number_game/src/main/java/task1/example/Main.java
@@ -9,14 +9,20 @@ public class Main {
 
         int randomNum = random.nextInt(1,100);
         int user_guess = 0;
+        int guess_count = 0;
         while (user_guess != randomNum){
             System.out.println("Guess the number generated");
             user_guess = sc.nextInt();
-            
+            guess_count++;
             if (user_guess > randomNum){
                 System.out.println("Your guess is too high! Try again.");
             } else if (user_guess < randomNum){
                 System.out.println("Your guess is too low! Try again.");
+            }
+
+            if (guess_count == 3){
+                System.out.printf("\nYou have reached your limit of guesses %d", guess_count);
+                break;
             }
         }
 
