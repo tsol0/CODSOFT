be735a96807e5ff1031f9f0d34cec34e98ed5821
Tsolo Khambule <tskhamb022@student.wethinkcode.co.za>
2024-03-09 04:18:25 +0200

    bases for my atm interface

---
+++ atm_interface/src/main/java/task3/atm/Main.java
@@ -0,0 +1,7 @@
+package task3.atm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ atm_interface/src/main/java/task3/atm/atm.java
@@ -0,0 +1,8 @@
+package task3.atm;
+
+public interface atm {
+
+    double withdrawal(double amount);
+    double deposit(double amount);
+    double checkBalance();
+}
